4b0cd12ec5508cc19954060a81f23d025cdbdc00
Nurse Soraka <60757347+LightshieldDotDev@users.noreply.github.com>
2020-04-16 20:15:24 +0200

    Removed unused code

--- league_updater/run.py
+++ league_updater/run.py
@@ -151,92 +151,6 @@ class Publisher(WorkerThread):
         print("Received interrupt. Shutting down.")
         connection.close()
 
-
-async def process_entries(message):
-
-    data = json.loads(message.body.decode())
-    print(f"Adding {len(data)} players to the db.")
-    if len(data) == 0:
-        return False
-    for player in data:
-        player_database = await asyncio.get_event_loop().run_in_executor(
-            None,
-            Player.objects.filter(
-                server=settings.SERVER,
-                summoner_id=player['summonerId']).first)
-
-        if not player_database:
-            player_database = Player(
-                server=settings.SERVER,
-                summoner_id=player['summonerId'])
-        await player_database.update(player)
-        await asyncio.get_event_loop().run_in_executor(
-            None,
-            player_database.save)
-
-    return True
-
-def parse_message(channel, method_frame, header_frame, body):
-
-    print(method_frame.delivery_tag)
-    print(header_frame)
-    print(body)
-    print()
-
-    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-
-
-async def update_tier(counter, channel, queue_out, queue_return):
-
-    has_content = True
-    start_page = 1
-    empties = 0
-    while has_content:
-        ids = []
-        for i in range(start_page, counter.pages+1):
-            message_body = {
-                    "method": "entries",
-                    "params": {
-                        'queue': counter.queue,
-                        'tier': counter.tier,
-                        'division': counter.division,
-                        'page': i
-                    }
-                }
-            print(message_body)
-            await channel.default_exchange.publish(
-
-                aio_pika.Message(
-                    headers={'return': settings.SERVER + "_league", 'id': str(i)},
-                    body=json.dumps(message_body).encode()
-                ),
-                routing_key=settings.SERVER + '_LEAGUE_RET'
-            )
-            ids.append(str(i))
-        print(ids)
-        while ids:
-            try:
-                message = await queue_return.get(timeout=5)
-                await message.ack()
-                info = message.info()
-                print(info)
-                print(f"Waiting for {len(ids)} IDs.")
-                ids.pop(ids.index(message.headers_raw['id'].decode()))
-                if not await process_entries(message):
-                    has_content = False
-                    empties += 1
-            except Exception as err:
-                print(err)
-                await asyncio.sleep(0.5)
-        if has_content:
-            start_page = counter.pages+1
-            counter.pages += 10
-        else:
-            counter.pages -= empties
-        await asyncio.get_event_loop().run_in_executor(
-            None,
-            counter.save)
-
 def main():
 
     # Create initial pages if none exist:
@@ -272,6 +186,7 @@ def main():
         publisher.stop()
     listener.join()
     publisher.join()
+
 
 if __name__ == "__main__":
     main()
