5eaed5b6086913988229554e9008727374bfc565
Nurse Soraka <60757347+LightshieldDotDev@users.noreply.github.com>
2020-05-16 15:31:44 +0200

    Updated manager to allow multiple server

--- manager/run.py
+++ manager/run.py
@@ -13,9 +13,11 @@ api_key = json.loads(open("secrets.json").read())['API_KEY']
 
 from server import ServerManager
 
+
 def end_handler(sig, frame):
     """Translate the docker stop signal SIGTERM to KeyboardInterrupt."""
     raise KeyboardInterrupt
+
 
 def main():
     # Startup Process - General
@@ -33,8 +35,12 @@ def main():
     rabbitmq.startup(wait_for="0.0.0.0:5672")
 
     # Startup Process - Depending on Config
-    euw = ServerManager("EUW1", client, rabbitmq, api_key, config)
-    euw.start()
+    server_threads = {}
+    for server in config['server']:
+        if config['server'][server]:
+            server_threads[server] = ServerManager(server, client, rabbitmq,
+                                                   api_key, config)
+            server_threads[server].start()
 
     # Permanent loop
     try:
@@ -43,8 +49,10 @@ def main():
             time.sleep(5)
     except KeyboardInterrupt:
         print("Received Keyboard interrupt")
-        euw.stop()
-    euw.join()
+        for thread in server_threads:
+            thread.stop()
+    for thread in server_threads:
+        thread.join()
 
 
 if __name__ == "__main__":
