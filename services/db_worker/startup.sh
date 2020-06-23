f4043db795100a719aaeda0867f30521665ae10a
Nurse Soraka <60757347+LightshieldDotDev@users.noreply.github.com>
2020-06-23 12:39:20 +0200

    Startup sequence fixed 3/?

--- services/db_worker/startup.sh
+++ services/db_worker/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./wait-for-it.sh rabbitmq:6572
+./wait-for-it.sh rabbitmq:5672
 ./wait-for-it.sh postgres:5432
 
 python -u run.py
--- services/league_updater/startup.sh
+++ services/league_updater/startup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 ./wait-for-it.sh proxy:8000
-./wait-for-it.sh rabbitmq:6572
+./wait-for-it.sh rabbitmq:5672
+
 python -u run.py
