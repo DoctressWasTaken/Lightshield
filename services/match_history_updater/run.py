6faeb58c09b6b69a3664720b1036a35e18bb5a9b
Nurse <60757347+LightshieldDotDev@users.noreply.github.com>
2020-06-26 17:06:58 +0200

    Output expanded.
    Removed unecessary segments

--- services/match_history_updater/run.py
+++ services/match_history_updater/run.py
@@ -89,39 +89,37 @@ class MatchHistoryUpdater(Worker):
         calls = int(matches_to_call / 100) + 1
         ids = [start_id * 100 for start_id in range(calls)]
         content = json.loads(msg.body.decode('utf-8'))
-        matches = []
-
+        calls_executed = []
         while ids:
-            calls_executed = []
-            while ids:
-                id = ids.pop()
-                calls_executed.append(asyncio.create_task(
-                    self.handler(
-                        session=session,
-                        url=self.url_template % (content['accountId'], id, id + 100))
-                ))
-            try:
-                responses = await asyncio.gather(*calls_executed)
-                matches = list(set().union(*responses))
-                await self.redis.hset(
-                    key=f"user:{identifier}",
-                    mapping={
-                        "summonerName": content['summonerName'],
-                        "wins": content['wins'],
-                        "losses": content['losses'],
-                        "tier": content['tier'],
-                        "rank": content['rank'],
-                        "leaguePoints": content['leaguePoints']
-                    }
-                )
-                while matches:
-                    id = matches.pop()
-                    await self.pika.push(id)
-                    await self.pika.ack(msg)
-            except NotFoundException:  # Triggers only if a call returns 404. Forces a full reject.
-                await self.pika.reject(msg, requeue=False)
-            finally:
-                del self.buffered_summoners[identifier]
+            id = ids.pop()
+            calls_executed.append(asyncio.create_task(
+                self.handler(
+                    session=session,
+                    url=self.url_template % (content['accountId'], id, id + 100))
+            ))
+            await asyncio.sleep(0.01)
+        try:
+            responses = await asyncio.gather(*calls_executed)
+            matches = list(set().union(*responses))
+            await self.redis.hset(
+                key=f"user:{identifier}",
+                mapping={
+                    "summonerName": content['summonerName'],
+                    "wins": content['wins'],
+                    "losses": content['losses'],
+                    "tier": content['tier'],
+                    "rank": content['rank'],
+                    "leaguePoints": content['leaguePoints']
+                }
+            )
+            while matches:
+                id = matches.pop()
+                await self.pika.push(id)
+                await self.pika.ack(msg)
+        except NotFoundException:  # Triggers only if a call returns 404. Forces a full reject.
+            await self.pika.reject(msg, requeue=False)
+        finally:
+            del self.buffered_summoners[identifier]
 
 
 if __name__ == "__main__":
