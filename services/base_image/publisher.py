c5789ffb51b71e34cb0c45ecb68767453a193e91
LightshieldDotDev <robinholzwarth95@gmail.com>
2020-10-08 21:23:14 +0200

    Another fixed output

--- services/base_image/publisher.py
+++ services/base_image/publisher.py
@@ -153,4 +153,4 @@ class Publisher(threading.Thread):
         finally:
             del self.client_names[client_name]
             self.clients.remove(ws)
-        self.logging.info("Client %s closed connection." % client_name)
+            self.logging.info("Client %s closed connection." % client_name)
