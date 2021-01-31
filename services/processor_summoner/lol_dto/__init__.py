7aeae4115261401999c3c6b0554cfa4f2f29cd46
Robin <robinholzwarth95@gmail.com>
2021-01-31 16:56:57 +0100

    Moved lol_dto to a super position to be used as volume in both container.

--- services/processor_summoner/lol_dto/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Base
-from .player import Player, Runes
-from .team import Team
-from .match import Match
-from .summoner import Summoner
--- services/processor_summoner/lol_dto/lol_dto/base.py
+++
@@ -1,4 +0,0 @@
-"""Create declarative base used to initiate all lol_dto."""
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
