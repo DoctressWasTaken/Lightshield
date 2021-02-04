c4982d922e0f8e28ba7dbc3b4bc632d4d37434cb
Robin <robinholzwarth95@gmail.com>
2021-02-04 20:53:37 +0100

    Added comma

--- postgres/summoner.sql
+++ postgres/summoner.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS summoner (
 
     summoner_id VARCHAR(63) PRIMARY KEY,
     account_id VARCHAR(56),
-    puuid VARCHAR(78)
+    puuid VARCHAR(78),
 
     rank SMALLINT,
     rank_history SMALLINT[],
