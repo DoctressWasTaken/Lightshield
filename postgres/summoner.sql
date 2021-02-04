802e7c63c6b77e709383dc5c03ab7078abb37492
Robin <robinholzwarth95@gmail.com>
2021-02-04 22:45:47 +0100

    Added summoner_ids_manager

--- postgres/summoner.sql
+++ postgres/summoner.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS summoner (
 
-    summoner_id VARCHAR(63) PRIMARY KEY,
-    account_id VARCHAR(56),
-    puuid VARCHAR(78),
+                                        summoner_id         VARCHAR(63) PRIMARY KEY,
+                                        account_id          VARCHAR(56),
+                                        puuid               VARCHAR(78),
 
-    rank SMALLINT,
-    rank_history SMALLINT[],
+                                        rank                SMALLINT,
+                                        rank_history        SMALLINT[],
 
-    wins SMALLINT,
-    wins_last_updated SMALLINT,
+                                        wins                SMALLINT,
+                                        wins_last_updated   SMALLINT,
 
-    losses SMALLINT,
-    losses_last_updated SMALLINT,
+                                        losses              SMALLINT DEFAULT 0,
+                                        losses_last_updated SMALLINT DEFAULT 0,
 
-    priority VARCHAR(1)
+                                        priority            VARCHAR(1)
+                                            checked_out TIMESTAMP
 );
 
 
---
+++ services/summoner_ids_manager/run.py
@@ -0,0 +1,56 @@
+import asyncio
+import signal
+from datetime import datetime, timedelta
+
+import aioredis
+import asyncpg
+
+
+class Manager:
+    stopped = False
+
+    async def init(self):
+        self.aioredis = await aioredis.create_connection(
+            ('redis', 6379))
+
+    def shutdown(self):
+        self.stopped = True
+
+    async def run(self):
+        await self.init()
+
+        while not self.stopped:
+            # Drop timed out tasks
+            limit = (datetime.utcnow() - timedelta(minutes=10)).total_seconds()
+            self.aioredis.zremrangebyscore('in_progress', max=limit)
+            # Check remaining buffer size
+            if self.aioredis.llen('tasks') < 250:
+                # Pull new tasks
+                conn = await asyncpg.connect("postgresql://postgres@postgres/raw")
+                result = await conn.fetch('''
+                    SELECT summoner_id
+                    FROM summoner
+                    WHERE account_id IS NULL
+                    LIMIT 500;
+                    ''')
+                # Add new tasks
+                for entry in result:
+                    if self.aioredis.lpush('task', entry['summoner_id']) >= 500:
+                        break
+
+            await asyncio.sleep(10)
+
+
+async def main():
+    manager = Manager()
+
+    def shutdown_handler():
+        """Shutdown."""
+        manager.shutdown()
+
+    signal.signal(signal.SIGTERM, shutdown_handler)
+    await manager.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
