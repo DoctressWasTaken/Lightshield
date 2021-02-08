2aa08e79a11329d607a0f68f3401fa44178445a3
Robin <robinholzwarth95@gmail.com>
2021-02-08 22:38:35 +0100

    Fixed image names

--- postgres/match.sql
+++ postgres/match.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS match
 (
-    match_id  BIGINT PRIMARY KEY,
-    queue     SMALLINT,
-    timestamp TIMESTAMP,
-    duration  SMALLINT DEFAULT NULL,
-    WIN       BOOLEAN  DEFAULT NULL
+    match_id        BIGINT PRIMARY KEY,
+    queue           SMALLINT,
+    timestamp       TIMESTAMP,
+    duration        SMALLINT DEFAULT NULL,
+    WIN             BOOLEAN  DEFAULT NULL, -- False: Blue | True: Red
+    details_pulled  BOOLEAN,
+    timeline_pulled BOOLEAN
 )
