ac0bd333ff199850747a0bf6ff4c54969aa802f6
Robin <robinholzwarth95@gmail.com>
2021-02-14 18:36:03 +0100

    Encoded lists as string

--- services/match_details/logic.py
+++ services/match_details/logic.py
@@ -93,7 +93,7 @@ class Service:
                     team_sets.append((
                         int(match[0]),
                         team['teamId'] == 200,
-                        bans,
+                        str(bans),
                         team['towerKills'],
                         team['inhibitorKills'],
                         team['firstTower'],
@@ -115,7 +115,7 @@ class Service:
                         int(match[0]),
                         participant['participantId'],
                         participant['player']['summonerId'],
-                        [participant['spell1Id'], participant['spell2Id']],
+                        str([participant['spell1Id'], participant['spell2Id']]),
                         tree_ids[(participant['stats']['perkPrimaryStyle'] - 8000) // 100 - 1],
                         tree_ids[(participant['stats']['perkSubStyle'] - 8000) // 100 - 1],
                         self.rune_ids[participant['stats']['perk0']] +
@@ -127,11 +127,11 @@ class Service:
                         shard_id[participant['stats']['statPerk0']] * 100 +
                         shard_id[participant['stats']['statPerk1']] * 10 +
                         shard_id[participant['stats']['statPerk2']],
-                        [
+                        str([
                             participant['stats']['item0'], participant['stats']['item1'],
                             participant['stats']['item2'], participant['stats']['item3'],
                             participant['stats']['item4'], participant['stats']['item5'],
-                        ],
+                        ]),
                         participant['stats']['item6'],
                         participant['stats']['champLevel'],
                         participant['championId'],
