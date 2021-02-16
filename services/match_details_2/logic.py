ff4f19c43243b4bfea65b5b4845cc4eda2eb8cc4
Robin <robinholzwarth95@gmail.com>
2021-02-16 19:07:07 +0100

    .

--- services/match_details_2/logic.py
+++ services/match_details_2/logic.py
@@ -186,8 +186,8 @@ class Service:
                     lines.append(
                         template % tuple([str(param) if type(param) in (list, bool) else param for param in line]))
                 values = ",".join(lines)
-                query = '''
-                INSERT INTO participant
+                template = '''
+                                INSERT INTO participant
                 (match_id, participant_id, summoner_id, summoner_spell,
                  rune_main_tree, rune_sec_tree, rune_main_select,
                  rune_sec_select, rune_shards, item, -- 10 
@@ -199,10 +199,13 @@ class Service:
                  damage_mitigated, physical_dealt, magical_dealt, 
                  true_dealt, turret_dealt, objective_dealt, total_heal,
                  total_units_healed, time_cc_others, total_cc_dealt)
-                VALUES %s
-                ON CONFLICT DO NOTHING;
-                ''' % values.replace('[', '{').replace(']', '}')
-                await conn.execute(query)
+                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,
+                        $11,$12,$13,$14,$15,$16,$17,$18,$19,$20,
+                        $21,$22,$23,$24,$25,$26,$27,$28,$29,$30,
+                        $31,$32,$33,$34,$35,$36,$37,$38)
+                '''
+                participant_insert = await conn.prepare(template)
+                await participant_insert.executemany(participant_sets)
                 self.logging.info("Inserted %s participant entries.", len(participant_sets))
             if update_sets:
                 await conn.executemany('''
