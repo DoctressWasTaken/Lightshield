e31430ce9d98984470500a908be0d9aa3e8fbef7
Robin <robinholzwarth95@gmail.com>
2022-02-06 20:23:43 +0100

    Catching queueId == 0 matches

--- services/match_details/service.py
+++ services/match_details/service.py
@@ -141,14 +141,16 @@ class Platform:
             #   Success
             url = self.endpoint_url % (params["platform"], params["match_id"])
             data = await self.endpoint.request(url, session)
+            if data['info']['queueId'] == 0:
+                raise NotFoundException
             self.result_details.append(
                 [params["platform"], params["match_id"], json.dumps(data)]
             )
             # self.logging.debug(url)
             if "gameStartTimestamp" in data["info"]:
                 game_duration = (
-                        data["info"]["gameStartTimestamp"]
-                        - data["info"]["gameStartTimestamp"]
+                    data["info"]["gameStartTimestamp"]
+                    - data["info"]["gameStartTimestamp"]
                 )
             else:
                 game_duration = data["info"]["gameDuration"]
@@ -181,7 +183,6 @@ class Platform:
             self.result_not_found.append([params["platform"], params["match_id"]])
         except Exception as err:
             self.logging.error("General: %s", err)
-            self.logging.info(data)
             return params
 
     async def worker(self):
@@ -201,7 +202,7 @@ class Platform:
                     break
                 targets.append(self.tasks.pop())
             async with aiohttp.ClientSession(
-                    headers={"X-Riot-Token": self.handler.api_key}
+                headers={"X-Riot-Token": self.handler.api_key}
             ) as session:
                 targets = [
                     target
