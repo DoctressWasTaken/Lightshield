85b40b148ae906eea12421f4b7f3c1bb5a748f1a
Doctress <robinholzwarth95@gmail.com>
2022-07-14 21:13:21 +0200

    Comma

--- lightshield/__init__.py
+++ lightshield/__init__.py
@@ -23,7 +23,7 @@ default_services = (
     "match_history",
     "match_details",
     "match_timeline",
-    "puuid_collector.rabbitmq.tasks"
+    "puuid_collector.rabbitmq.tasks",
     "puuid_collector.rabbitmq.results"
 )
 
