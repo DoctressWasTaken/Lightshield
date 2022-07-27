64d4e4d17bbab6a5976271362a92699ba6072f3f
Doctress <robinholzwarth95@gmail.com>
2022-07-28 01:42:42 +0200

    Match Details results updated to receive the 10x packs of summoners instead of individual ones
    Bugfix

--- lightshield/services/match_details/rabbitmq/results.py
+++ lightshield/services/match_details/rabbitmq/results.py
@@ -67,7 +67,6 @@ class Handler:
         tasks = self.buffered_tasks[platform]["matches_404"].copy()
         self.buffered_tasks[platform]["matches_404"] = []
         tasks = [[int(task.decode("utf-8"))] for task in tasks]
-        self.logging.error(tasks)
         async with self.db.acquire() as connection:
             prep = await connection.prepare(
                 queries.flush_missing[self.config.database].format(
