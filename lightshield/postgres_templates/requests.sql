a96fbee7295dc761a4c72cc7c5a23e501ed5063d
Doctress <robinholzwarth95@gmail.com>
2022-08-08 13:31:48 +0200

    Added requests template for postgres

---
+++ lightshield/postgres_templates/requests.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS tracking;
+CREATE TABLE tracking.requests
+(
+    response_type SMALLINT,
+    request_count INT,
+    interval_time TIMESTAMP,
+    platform      platform,
+    endpoint      VARCHAR,
+    api_key       VARCHAR,
+    PRIMARY KEY (response_type, interval_time, platform, endpoint, api_key)
+);
