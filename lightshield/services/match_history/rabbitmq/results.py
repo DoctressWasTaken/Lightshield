3e4cf56b405139393bc9b8989dd897c460195328
Doctress <robinholzwarth95@gmail.com>
2022-08-10 17:19:15 +0200

    Match History updated
    - Bugs?

--- lightshield/services/match_history/rabbitmq/results.py
+++ lightshield/services/match_history/rabbitmq/results.py
@@ -63,7 +63,7 @@ class Handler:
             else:
                 if match_platform not in tasks_2:
                     tasks_2[match_platform] = []
-                tasks_2 += package
+                tasks_2[match_platform] += package
             counter += 1
         self.logging.info(" %s\t | %s matches inserted", platform,counter)
         async with self.db.acquire() as connection:
